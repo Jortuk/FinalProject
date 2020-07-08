45c9c79250ca5af6d3da1dede9063223e0a47e9a
Ubuntu <ubuntu@ip-172-31-24-8.eu-west-1.compute.internal>
2020-07-08 10:13:11 +0000

    jenkins

--- script/docker.sh
+++ script/docker.sh
@@ -9,11 +9,11 @@ sudo docker push t2fp/backend:latest
 
 
 
-curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
-kubectl version --short --client
+#curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl
+#chmod +x ./kubectl
+#mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+#echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+#kubectl version --short --client
 source ~/.bashrc
 #aws eks update-kubeconfig --name example
 
