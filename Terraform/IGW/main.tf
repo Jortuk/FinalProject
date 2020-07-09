a1a6ebe1af9ba1d441ae9453c66234418468571e
Ubuntu <ubuntu@ip-172-31-40-11.eu-west-1.compute.internal>
2020-07-09 23:56:37 +0000

    jenkins

--- Terraform/IGW/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = var.vpc_id
-
-  tags = {
-    Name    = var.igw_name
-    Network = var.igw_network
-  }
-}
--- Terraform/ROUTES/ASSOCIATION/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "subnet_id" {
-  description = "This uses the VPC id aquired from the VPC Module to map this Tbale to the correct Network."
-}
-
-variable "table_id" {
-  description = "This is the Table's ID."
-}
