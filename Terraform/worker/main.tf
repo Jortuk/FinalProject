dc975c3ce26ad9d6762f5752777e4841914c7313
SeanSnake93 <seansnake93@hotmail.co.uk>
2020-07-09 10:07:06 +0100

    Terra Update 09/07/2020@10:07

--- Terraform/worker/main.tf
+++ Terraform/worker/main.tf
@@ -19,14 +19,29 @@ module "key_pair" {
 # @@@@@@@       Workers       @@@@@@@
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
+
+data "aws_subnet" "subnet" {
+  filter {
+    name   = "tag:Name"
+    values = ["PetClinic_Subnet"]
+  }
+}
+
+data "aws_security_group" "sg" {
+  filter {
+    name   = "tag:Name"
+    values = ["multi_port_access"]
+  }
+}
+
 module "ec2_worker1" {
   source         = "./EC2"
   instance_count = 1
   ami_code       = "ami-008320af74136c628" # Ubuntu 16.04
   type_code      = "t2.small"              # 2 x CPU + 4 x RAM
   pem_key        = "petClinic"
-  subnet         = module.subnet.id # Task ~ get the Subnet group with Name Tag "PetClinic_Subnet"
-  vpc_sg         = [module.sg.id] # Task ~ get the Security group with Name Tag "multi_port_access"
+  subnet         = data.aws_subnet.subnet.id # Task ~ get the Subnet group with Name Tag "PetClinic_Subnet"
+  vpc_sg         = [data.aws_security_group.sg.id] # Task ~ get the Security group with Name Tag "multi_port_access"
   pub_ip         = true
   user_data      = <<-EOF
   #! /bin/bash
